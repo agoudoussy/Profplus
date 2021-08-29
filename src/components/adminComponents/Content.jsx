d176e4ef6e5c1352f1d8bf7d753d68fdafbcb98d
ibrah25.b@gmail.com <ibrah25.b@gmail.com>
2021-08-29 00:28:00 +0000

    content updated

--- src/components/adminComponents/Content.jsx
+++ src/components/adminComponents/Content.jsx
@@ -6,50 +6,58 @@ function content() {
   return (
     <div className="admin-content">
       <AdminHead />
+      <div className="generalB">
       <div className="personal__info">
-        <h4>Information Personnelle</h4>
-        <div>
+        <h4 className="title_brand">Information Personnelle</h4>
+        <div className="inputSize">
           <input placeholder="nom" type="text" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="prenom" type="text" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="telephone" type="text" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="email" type="text" />
         </div>
       </div>
+      <div className="profile">
+            <input type="file"/>
+            <button className="botumValidate">Enregistrer</button>
+      </div>
+      </div>
       <div className="residence_info">
-        <h4>Information Residentielle</h4>
-        <div>
+        <h4 className="title_brand">Information Residentielle</h4>
+        <div className="inputSize">
           <input placeholder="categorie" type="text" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="libellé" type="text" />
         </div>
+        <button className="botumValidate">Enregistrer</button>
       </div>
       <div className="profosional_info">
-        <h4>Information Personnelle</h4>
-        <div>
+        <h4 className="title_brand">Information Profossionelle</h4>
+        <div className="text__area">
           <textarea placeholder="Presentation" />
         </div>
-        <div>
+        <div className="text__area">
           <textarea placeholder="experience profossionelle" />
         </div>
-        <div>
+        <div className="text__area">
           <textarea placeholder="experience social" />
         </div>
-        <div>
+        <div className="text__area">
           <textarea placeholder="citation" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="cout Horaire" type="text" />
         </div>
-        <div>
+        <div className="inputSize">
           <input placeholder="Annee d'experience" type="text" />
         </div>
+        <button className="botumValidate">Enregistrer</button>
       </div>
     </div>
   );
